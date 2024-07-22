facd4941a142d139ff35a98faf18671eabcdb3a9
MartinFiori <martinfiori@outlook.com>
2024-07-22 20:11:54 -0300

    nuevos estilos de las cards

--- src/Components/AnimalCard/AnimalCard.tsx
+++ src/Components/AnimalCard/AnimalCard.tsx
@@ -15,26 +15,27 @@ export default function AnimalCard({
   pair,
 }: Props): ReactElement {
   return (
-    // <div className="flip-card">
-    //   <div className="flip-card-inner">
-    //     <div className="flip-card-front">
-    //       <img src={`/assets/animals${animal.src}`} alt={animal.name} />
-    //     </div>
-    //     <div className="flip-card-back">
-    //       <h1>John Doe</h1>
-    //       <p>Architect & Engineer</p>
-    //       <p>We love that guy</p>
-    //     </div>
-    //   </div>
-    // </div>
-    <section
-      onClick={() => handleSetPairs(animal)}
-      className={`animal-card ${animal.match_found ? "match" : ""} ${
+    <div
+      className={`flip-card ${animal.match_found ? "match" : ""} ${
         pair.some((p) => p.id === animal.id) ? "selected" : ""
-      } `}
+      }`}
+      onClick={() => handleSetPairs(animal)}
     >
-      <img src={`/assets/animals${animal.src}`} alt={animal.name} />
-      <h2>{animal.name}</h2>
-    </section>
+      <div className="flip-card-inner">
+        <div className="flip-card-front"></div>
+        <div className="flip-card-back">
+          <img src={`/assets/animals${animal.src}`} alt={animal.name} />
+        </div>
+      </div>
+    </div>
+    // <section
+    //   onClick={() => handleSetPairs(animal)}
+    //   className={`animal-card ${animal.match_found ? "match" : ""} ${
+    //     pair.some((p) => p.id === animal.id) ? "selected" : ""
+    //   } `}
+    // >
+    //   <img src={`/assets/animals${animal.src}`} alt={animal.name} />
+    //   <h2>{animal.name}</h2>
+    // </section>
   );
 }
